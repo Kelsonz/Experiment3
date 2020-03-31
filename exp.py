ddeb352e33f0ea267aefa5036a24403d3708524d
Kelsonz Fu <kelsonz@icloud.com>
2020-03-31 09:08:54 +0800

    1 增加必要注释 2 ij换名 3 规范化所有流程，增加坐标变换这一步

--- exp.py
+++ exp.py
@@ -11,40 +11,51 @@ class exp:
         self.xsize, self.ysize = self.im.size
         self.rgb = list(self.im.getdata())
 
-    def set_data(self, x, y, data):
-        index = x * self.new_xsize + y
+    # 给新通道赋值
+    def set_data(self, y, x, data):
+        index = y * self.new_xsize + x
         self.new_rgb[index] = data
 
-    def get_data(self, x, y):
-        index = x * self.xsize + y
+    # 获得旧通道的值
+    def get_data(self, y, x):
+        index = y * self.xsize + x
         return self.rgb[index]
 
+    # 保存图像
     def save(self, item):
         self.new_im.putdata(self.new_rgb)
         self.new_im.save(item + self.filename)
         self.new_im.show()
 
+    # 生成新图像
     def new_image(self, xsize, ysize):
         self.new_xsize = xsize
         self.new_ysize = ysize
         self.new_im = Image.new('RGB', (xsize, ysize))
         self.new_rgb = [(0, 0, 0)] * (xsize * ysize)
 
+    # 旋转
     def rotate(self, degree):
+        # 计算旋转矩阵的cos, sin值
+        # 由于是从新图像往回推所以角度取负值
         cos = math.cos(math.radians(-degree))
         sin = math.sin(math.radians(-degree))
+        # 计算新画布大小
         d = int(math.sqrt(math.pow(self.xsize, 2) + math.pow(self.ysize, 2)))
         x = y = d
+        # 生成用于旋转的新图像
         self.new_image(x, y)
         for j in range(y):
             for i in range(x):
+                # 计算在原图中的坐标
                 X = (i - x / 2) * cos + (j - y / 2) * sin + self.xsize / 2
                 Y = -(i - x / 2) * sin + (j - y / 2) * cos + self.ysize / 2
-                if X >= self.xsize - 1 or Y >= self.ysize - 1:
+                if X >= self.xsize - 1 or Y >= self.ysize - 1 or X < 0 or Y < 0:
                     pass
-                elif X < 0 or Y < 0:
-                    pass
+                elif int(X) == X and int(Y) == Y:
+                    self.set_data(j, i, self.get_data(Y, X))
                 else:
+                    # 若生成坐标为小数，使用双线性插值的结果
                     self.deltax = X - int(X)
                     self.deltay = Y - int(Y)
                     xx = int(X) + 1
@@ -55,46 +66,62 @@ class exp:
                     xy11 = self.get_data(int(Y), xx)
                     rgb = self.rgb_double_interpolating(xy00, xy01, xy10, xy11)
                     self.set_data(j, i, rgb)
-
+        # 保存生成的图像
         self.save('rotate_' + str(degree) + '_')
 
+    # 图像反转
     def flip(self):
         self.new_image(self.xsize, self.ysize)
-        for i in range(self.ysize):
-            for j in range(self.xsize):
-                self.set_data(i, j, self.get_data(i, self.xsize - j - 1))
+        for j in range(self.ysize):
+            for i in range(self.xsize):
+                X = self.xsize - 1 - i
+                Y = j
+                self.set_data(j, i, self.get_data(Y, X))
         self.save('flip_')
 
+    # 图像缩放
     def resize(self, new_x, new_y):
         cx = self.xsize / new_x
         cy = self.ysize / new_y
         self.new_image(new_x, new_y)
-        for i in range(new_y):
-            for j in range(new_x):
-                self.set_data(i, j, self.get_data(int(cy * i), int(cx * j)))
+        for j in range(new_y):
+            for i in range(new_x):
+                X = int(cx * i)
+                Y = int(cy * j)
+                self.set_data(j, i, self.get_data(Y, X))
         self.save('resize_')
 
+    # 图像平移
     def move(self, x, y):
         x = int(x)
         y = int(y)
         self.new_image(x + self.xsize, y + self.ysize)
-        for i in range(self.ysize):
-            for j in range(self.xsize):
-                self.set_data(i + y, j + x, self.get_data(i, j))
+        for j in range(self.ysize + y):
+            for i in range(self.xsize + x):
+                X = i - x
+                Y = j - y
+                # 空出黑边
+                if X >= self.xsize - 1 or Y >= self.ysize - 1 or X < 0 or Y < 0:
+                    pass
+                else:
+                    self.set_data(j, i, self.get_data(Y, X))
         self.save('move_')
 
+    # RGB通道双线性插值
     def rgb_double_interpolating(self, xy00, xy01, xy10, xy11):
         r = int(self.func(xy00[0], xy01[0], xy10[0], xy11[0]))
         g = int(self.func(xy00[1], xy01[1], xy10[1], xy11[1]))
         b = int(self.func(xy00[2], xy01[2], xy10[2], xy11[2]))
         return (r, g, b)
 
+    # 双线性插值
     def func(self, xy00, xy01, xy10, xy11):
         a = (1 - self.deltax) * xy00 + self.deltax * xy01
         b = (1 - self.deltax) * xy10 + self.deltax * xy11
         return (1 - self.deltay) * b + self.deltay * a
 
 
+# 打印矩阵
 def matrix(matrix, xsize, ysize):
     for j in range(ysize):
         t = list()
@@ -104,7 +131,7 @@ def matrix(matrix, xsize, ysize):
 
 
 t = exp('color.jpg')
-t.rotate(40.52)
+# t.rotate(40.52)
 # t.flip()
-# t.move(20.2, 35.8)
+t.move(200.2, 305.8)
 # t.resize(1000, 640)
